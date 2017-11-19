fa1d7d764a83fa59ad8b302b6834170b4ae2b443
Frnkovich <mrfrank01@yandex.ru>
2017-11-19 19:13:59 +0300

    Добавил вывод гистограмм

---
+++ js/stat.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var columnWidth = 40;
+var columnSpacer = 50;
+var hystogramHeight = 150;
+var spacerHeight = 4;
+var textHeight = 16;
+var textX = 120;
+var textY = 32;
+var spaceLeft = 150;
+
+window.renderStatistics = function (ctx, names, times){
+	ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+	ctx.fillRect(110, 20, 420, 270);
+	
+	ctx.fillStyle = 'white';
+	ctx.fillRect(100, 10, 420, 270);
+	
+	ctx.fillStyle = 'black';
+	ctx.font = '16px PT Mono';
+	ctx.textBaseline = 'hanging';
+	ctx.fillText('Ура вы победили!', textX, textY);
+	ctx.fillText('Список результатов:', textX, textY + textHeight);
+	
+	var bottomMargin= spacerHeight + 5 * textHeight  + hystogramHeight;
+	
+	for(var i = 0; i<times.length; i++) {
+		var columnHeight = times[i] / maxResult(times) * hystogramHeight;
+		var columnY = bottomMargin - columnHeight;
+		var scoreY = columnY  - (spacerHeight + textHeight);
+		var columnX = 150 + (columnWidth + columnSpacer) * i;
+		
+		ctx.fillStyle = setColor(names[i]);
+		ctx.fillRect(columnX, columnY, columnWidth, columnHeight);
+		
+		ctx.fillStyle = 'black';
+		ctx.fillText(Math.round(times[i]), columnX, columnY - textHeight);
+		ctx.fillText(names[i], columnX, columnY + spacerHeight +  columnHeight);
+  }
+}
+
+var setColor = function(name){
+	var color;
+	var rand = Math.random();
+	if (name == "Вы"){
+		return color = 'rgba(255, 0, 0, 1)';
+	  }
+	else return color = 'rgba(0, 0, 255,' + rand + ')';
+}
+
+var maxResult = function(times){
+	var max = times[0];
+	for (var i = 0; i <= times.length; i++){
+		if (max < times[i])  max = times[i];
+	}
+	return max;
+}
+
