38c3bf175e25a39c053a876cb5372fcf231ca3fc
Frnkovich <mrfrank01@yandex.ru>
2017-11-19 23:17:44 +0300

    Разбил renderStatiscs на несколько функций

--- js/stat.js
+++ js/stat.js
@@ -10,49 +10,57 @@ var textY = 32;
 var spaceLeft = 150;
 
 window.renderStatistics = function (ctx, names, times){
-	ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-	ctx.fillRect(110, 20, 420, 270);
-	
-	ctx.fillStyle = 'white';
-	ctx.fillRect(100, 10, 420, 270);
-	
-	ctx.fillStyle = 'black';
-	ctx.font = '16px PT Mono';
-	ctx.textBaseline = 'hanging';
-	ctx.fillText('Ура вы победили!', textX, textY);
-	ctx.fillText('Список результатов:', textX, textY + textHeight);
-	
-	var bottomMargin= spacerHeight + 5 * textHeight  + hystogramHeight;
-	
-	for(var i = 0; i<times.length; i++) {
-		var columnHeight = times[i] / maxResult(times) * hystogramHeight;
-		var columnY = bottomMargin - columnHeight;
-		var scoreY = columnY  - (spacerHeight + textHeight);
-		var columnX = 150 + (columnWidth + columnSpacer) * i;
-		
-		ctx.fillStyle = setColor(names[i]);
-		ctx.fillRect(columnX, columnY, columnWidth, columnHeight);
-		
-		ctx.fillStyle = 'black';
-		ctx.fillText(Math.round(times[i]), columnX, columnY - textHeight);
-		ctx.fillText(names[i], columnX, columnY + spacerHeight +  columnHeight);
-  }
+  
+  renderCloud(ctx);
+  
+  ctx.fillStyle = 'black';
+  ctx.font = textHeight + 'px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили!', textX, textY);
+  ctx.fillText('Список результатов:', textX, textY + textHeight);
+
+  renderHistogram(ctx, names, times);  
 }
 
 var setColor = function(name){
-	var color;
-	var rand = Math.random();
-	if (name == "Вы"){
-		return color = 'rgba(255, 0, 0, 1)';
-	  }
-	else return color = 'rgba(0, 0, 255,' + rand + ')';
+  var color;
+  var rand = Math.random();
+  if (name == "Вы"){
+    return color = 'rgba(255, 0, 0, 1)';
+  }
+  else return color = 'rgba(0, 0, 255,' + rand + ')';
 }
 
 var maxResult = function(times){
-	var max = times[0];
-	for (var i = 0; i <= times.length; i++){
-		if (max < times[i])  max = times[i];
-	}
-	return max;
+  var max = times[0];
+  for (var i = 0; i <= times.length; i++){
+    if (max < times[i])  max = times[i];
+  }
+  return max;
 }
 
+var renderCloud = function(ctx){
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+  
+  ctx.fillStyle = 'white';
+  ctx.fillRect(100, 10, 420, 270);
+}
+
+var renderHistogram = function(ctx, names, times){
+  var bottomMargin= spacerHeight + 5 * textHeight  + hystogramHeight; 
+  
+  for(var i = 0; i<times.length; i++) {
+    var columnHeight = times[i] / maxResult(times) * hystogramHeight;
+	var columnY = bottomMargin - columnHeight;
+	var scoreY = columnY  - (spacerHeight + textHeight);
+	var columnX = spaceLeft + (columnWidth + columnSpacer) * i;
+	 
+	ctx.fillStyle = setColor(names[i]);
+    ctx.fillRect(columnX, columnY, columnWidth, columnHeight);
+	 
+	ctx.fillStyle = 'black';
+	ctx.fillText(Math.round(times[i]), columnX, columnY - textHeight);
+	ctx.fillText(names[i], columnX, columnY + spacerHeight +  columnHeight);
+  }	
+}
