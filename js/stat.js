2d89e6bbd68ad9b2c343cd62ce48cda4347fd579
Frnkovich <mrfrank01@yandex.ru>
2017-11-20 13:31:16 +0300

    Поправил названия переменных

--- js/stat.js
+++ js/stat.js
@@ -12,7 +12,7 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-var SHIFT_CLOUD = 10;
+var SHIFT_SHADOW = 10;
 var MIN_VISABILITY = 0.2;
 var MAX_VISABILITY = 1;
 
@@ -21,7 +21,7 @@ var setColor = function(name){
   if (name === "Вы"){
     return 'rgba(255, 0, 0, 1)';
   }
-  return 'rgba(0, 0, 255,' + getRandom(minVisability, maxVisability) + ')';
+  return 'rgba(0, 0, 255,' + getRandom(MIN_VISABILITY, MAX_VISABILITY) + ')';
 }
 
 var getMaxResult = function(times){
@@ -34,27 +34,27 @@ var getMaxResult = function(times){
 
 var renderCloud = function(ctx){
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(cloudX + shiftCloud, cloudY + shiftCloud, cloudWidth, cloudHeight);
+  ctx.fillRect(CLOUD_X + SHIFT_SHADOW, CLOUD_Y + SHIFT_SHADOW, CLOUD_WIDTH, CLOUD_HEIGHT);
   
   ctx.fillStyle = 'white';
-  ctx.fillRect(cloudX, cloudY, cloudWidth, cloudHeight);
+  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
 }
 
 var renderHistogram = function(ctx, names, times){
-  var bottomMargin = spacerHeight + 5 * textHeight  + hystogramHeight; 
+  var bottomMargin = SPACER_HEIGHT + 5 * TEXT_HEIGHT  + HYSTOGRAM_HEIGHT; 
   
   for(var i = 0; i<times.length; i++) {
-    var columnHeight = times[i] / getMaxResult(times) * hystogramHeight;
+    var columnHeight = times[i] / getMaxResult(times) * HYSTOGRAM_HEIGHT;
     var columnY = bottomMargin - columnHeight;
-    var scoreY = columnY  - (spacerHeight + textHeight);
-    var columnX = spaceLeft + (columnWidth + columnSpacer) * i;
+    var scoreY = columnY  - (SPACER_HEIGHT + TEXT_HEIGHT);
+    var columnX = SPACE_LEFT + (COLUM_NWIDTH + COLUMN_SPACER) * i;
 	 
     ctx.fillStyle = setColor(names[i]);
-    ctx.fillRect(columnX, columnY, columnWidth, columnHeight);
+    ctx.fillRect(columnX, columnY, COLUM_NWIDTH, columnHeight);
 	 
     ctx.fillStyle = 'black';
-    ctx.fillText(Math.round(times[i]), columnX, columnY - textHeight);
-    ctx.fillText(names[i], columnX, columnY + spacerHeight +  columnHeight);
+    ctx.fillText(Math.round(times[i]), columnX, columnY - TEXT_HEIGHT);
+    ctx.fillText(names[i], columnX, columnY + SPACER_HEIGHT +  columnHeight);
   }	
 }
 
@@ -67,10 +67,10 @@ window.renderStatistics = function (ctx, names, times){
   renderCloud(ctx);
   
   ctx.fillStyle = 'black';
-  ctx.font = textHeight + 'px PT Mono';
+  ctx.font = TEXT_HEIGHT + 'px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', textX, textY);
-  ctx.fillText('Список результатов:', textX, textY + textHeight);
+  ctx.fillText('Ура вы победили!', TEXT_X, TEXT_Y);
+  ctx.fillText('Список результатов:', TEXT_X, TEXT_Y + TEXT_HEIGHT);
 
   renderHistogram(ctx, names, times);  
 }
