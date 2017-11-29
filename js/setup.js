bcce9a8df650b40546248ed87ff8dc4acd7aa363
Frnkovich <mrfrank01@yandex.ru>
2017-11-30 00:35:22 +0300

    Поправил код по замечаниям

--- js/setup.js
+++ js/setup.js
@@ -7,31 +7,29 @@ var FIRSTNAMES_LIST = ['Иван', 'Хуан Себастьян', 'Мария', 
 var SURNAMES_LIST = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS_LIST = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS_LIST = ['black', 'red', 'blue', 'yellow', 'green'];
+var MAX_WIZARD = 3;
 var wizards = [];
 var similarListElement = setup.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 var getRandom = function (min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
 var getWizardData = function () {
-  var wizard = {
+  return wizard = {
     name: FIRSTNAMES_LIST[getRandom(0, FIRSTNAMES_LIST.length - 1)] + ' ' + SURNAMES_LIST[getRandom(0, SURNAMES_LIST.length - 1)],
     coatColor: COAT_COLORS_LIST[getRandom(0, COAT_COLORS_LIST.length - 1)],
     eyesColor: EYES_COLORS_LIST[getRandom(0, EYES_COLORS_LIST.length - 1)]
   };
-  return wizard;
 };
 
 var fillWizardsData = function () {
-  for (var i = 0; i <= 3; i++) {
-    wizards[i] = getWizardData();
+  for (var i = 0; i <= MAX_WIZARD; i++) {
+    wizards.push(getWizardData());
   }
   return wizards;
 };
-
-fillWizardsData();
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -41,10 +39,14 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
+var showWizardsOnScreen = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+  setup.querySelector('.setup-similar').classList.remove('hidden');
 }
-similarListElement.appendChild(fragment);
 
-setup.querySelector('.setup-similar').classList.remove('hidden');
+fillWizardsData();
+showWizardsOnScreen();
