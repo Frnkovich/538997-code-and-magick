63078a094933473d7cac4310767f58cec87b7bf3
Frnkovich <mrfrank01@yandex.ru>
2017-11-30 16:07:52 +0300

    Исправления по замечениям

--- js/setup.js
+++ js/setup.js
@@ -7,7 +7,7 @@ var FIRSTNAMES_LIST = ['Иван', 'Хуан Себастьян', 'Мария', 
 var SURNAMES_LIST = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS_LIST = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS_LIST = ['black', 'red', 'blue', 'yellow', 'green'];
-var MAX_WIZARD = 3;
+var MAX_WIZARD = 4;
 var wizards = [];
 var similarListElement = setup.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
@@ -17,7 +17,7 @@ var getRandom = function (min, max) {
 };
 
 var getWizardData = function () {
-  return wizard = {
+  return {
     name: FIRSTNAMES_LIST[getRandom(0, FIRSTNAMES_LIST.length - 1)] + ' ' + SURNAMES_LIST[getRandom(0, SURNAMES_LIST.length - 1)],
     coatColor: COAT_COLORS_LIST[getRandom(0, COAT_COLORS_LIST.length - 1)],
     eyesColor: EYES_COLORS_LIST[getRandom(0, EYES_COLORS_LIST.length - 1)]
@@ -25,7 +25,7 @@ var getWizardData = function () {
 };
 
 var fillWizardsData = function () {
-  for (var i = 0; i <= MAX_WIZARD; i++) {
+  for (var i = 0; i < MAX_WIZARD; i++) {
     wizards.push(getWizardData());
   }
   return wizards;
@@ -39,7 +39,7 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var showWizardsOnScreen = function () {
+var renderWizards = function () {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < wizards.length; i++) {
     fragment.appendChild(renderWizard(wizards[i]));
@@ -48,5 +48,9 @@ var showWizardsOnScreen = function () {
   setup.querySelector('.setup-similar').classList.remove('hidden');
 }
 
-fillWizardsData();
-showWizardsOnScreen();
+var renderALl = function () {
+  fillWizardsData();
+  renderWizards();
+}
+
+renderALl();
